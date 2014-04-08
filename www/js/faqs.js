224841f2601ecd6238bf5a3b990a7b10041c5300
Dylan Grant <dgrant069@hotmail.com>
2014-04-08 15:54:45 -0700

    FAQs mostly working, needs slight adjustments. video still not loading in about

--- www/js/faqs.js
+++ www/js/faqs.js
@@ -1,8 +1,20 @@
+// var theScroll;
+// function scroll(){
+//     theScroll = new iScroll('wrapper');
+// }
 
 
-var theScroll;
-function scroll(){
-    theScroll = new iScroll('wrapper');
-}
+// document.addEventListener('DOMContentLoaded', scroll, false);
 
-document.addEventListener('DOMContentLoaded', scroll, false);
+$(document).ready(function(){
+  $(function() {
+    $( "#faqs" ).accordion({
+      collapsible: true,
+      heightStyle: "content",
+      active: false
+    });
+  }),
+  $(".question").on("click", "dt", function(){
+    $(".question").css({ "height":"60px", "padding-bottom":"0px"});
+  });
+});
