a1a91d8234df16fbcd1cc2af7b4caa5a3bbf0e9d
Dylan Grant <dgrant069@hotmail.com>
2014-04-11 17:48:55 -0700

    moved templates into index. combining and condensing js files to application.js

--- www/js/application.js
+++ www/js/application.js
@@ -16,34 +16,106 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+// var app = {
+(function() {
+  // Application Constructor
+  var home = Handlebars.compile($("#home").html());
+  var faqs = Handlebars.compile($("#faqs").html());
+  var about = Handlebars.compile($("#about").html());
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+  // var faqsURL = /#faqs/
+  // var aboutURL = /#about/
 
-        console.log('Received Event: ' + id);
-    }
-};
+  // var slider = new PageSlider($('body'));
+
+  function initialize() {
+    this.bindEvents();
+  };
+
+  $(document).ready(function(){
+    $('body').html(new HomeView(home).render().el);
+  })
+  // $(window).on('hashchange', route);
+
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  function bindEvents() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  };
+
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicity call 'app.receivedEvent(...);'
+  function onDeviceReady() {
+    app.receivedEvent('deviceready');
+  };
+
+  // Update DOM on a Received Event
+  function receivedEvent(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+
+    console.log('Received Event: ' + id);
+  };
+
+  // route: function () {
+  //   var hash = window.location.hash;
+  //   var faqsMatch = hash.match(faqsURL);
+  //   if(!hash) {
+  //     slider.slidePage(new HomeView(home).render().el);
+  //     return;
+  //   },
+  //   if(faqsMatch) {
+  //     slider.slidePage(new FaqsView(faqs).render().el);
+  //   }
+  // }
+
+  var HomeView = function(template) {
+
+    this.initialize = function () {
+        // Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+    };
+
+    this.render = function() {
+        this.el.html(template);
+        return this;
+    };
+
+    this.initialize();
+  };
+
+  var FaqsView = function(template){
+
+    this.initialize = function(){
+      this.el = $('<div/>');
+      // this.el.html(faqs);
+      // return this;
+    };
+
+    this.initialize();
+
+    $(document).ready(function(){
+      $(function() {
+        $("#faqs").accordion({
+          collapsible: true,
+          heightStyle: "content",
+          active: false
+        });
+      });
+
+      $(".question").on("click", "dt", function(){
+        $(".question").css({ "height":"60px", "padding-bottom":"0px"});
+      });
+    });
+  };
+
+
+}());
--- www/js/faqs.js
+++ www/js/faqs.js
@@ -6,15 +6,42 @@
 
 // document.addEventListener('DOMContentLoaded', scroll, false);
 
-$(document).ready(function(){
-  $(function() {
-    $( "#faqs" ).accordion({
-      collapsible: true,
-      heightStyle: "content",
-      active: false
-    });
-  }),
-  $(".question").on("click", "dt", function(){
-    $(".question").css({ "height":"60px", "padding-bottom":"0px"});
-  });
-});
+// var FaqsView = function(template){
+
+//   this.render = function(){
+//     this.el.html(template());
+//     return this;
+//   };
+
+//   this.initialize() = function(){
+//     this.el = $('<div/>');
+//   };
+
+//   $(document).ready(function(){
+//     $(function() {
+//       $( "#faqs" ).accordion({
+//         collapsible: true,
+//         heightStyle: "content",
+//         active: false
+//       });
+//     }),
+
+//     $(".question").on("click", "dt", function(){
+//       $(".question").css({ "height":"60px", "padding-bottom":"0px"});
+//     });
+//   });
+// };
+
+var faqsContext = [
+  {question: "", answer: ""},
+  {question: "", answer: ""},
+  {question: "", answer: ""},
+  {question: "", answer: ""},
+  {question: "", answer: ""},
+  {question: "", answer: ""},
+  {question: "", answer: ""},
+  {question: "", answer: ""},
+  {question: "", answer: ""},
+  {question: "", answer: ""},
+
+]
